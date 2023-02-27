0987eb6b31effba8af16e3b37c87220d2437d028
SalmanSD07 <74087999+SalmanSD07@users.noreply.github.com>
2023-02-27 17:16:01 +0000

    Update db.js

--- src/api/v1/config/db.js
+++ src/api/v1/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 db = mongoose.connect(
-  "mongodb+srv://root:root@cluster0.9ypp5rj.mongodb.net/cupouchPets?retryWrites=true&w=majority",
+  "mongodb+srv://salman:salman@cluster0.9ypp5rj.mongodb.net/cupouchPets?retryWrites=true&w=majority",
   (err) => {
     console.log("Database connected");
     if (err) {
